bb640d4e3e166a50ad4e11a5642053a76d3d7d61
Enrique Silva <esilvam1580@gmail.com>
2025-06-05 22:18:17 -0600

    Se crea el componente Contador

---
+++ src/components/Contador.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Hola, bienvenido a Vue</h1>
+  <p>Contador: {{ contador }}</p>
+
+  <button @click="incrementar">Incrementar</button>
+  <button @click="decrementar">Decrementar</button>
+  <button @click="resetear">Resetear</button>  
+
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const contador = ref(0);
+
+function incrementar() {
+  contador.value++;
+}
+function decrementar() {
+  contador.value--;
+}
+function resetear() {
+  contador.value = 0;
+}
+
+
+</script>
+
+<style scoped>
+p {
+  font-size: 20px;
+  color: red;
+}
+
+</style>
