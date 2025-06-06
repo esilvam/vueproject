393eb0dc566d91e6fbcc11d91e4b4e65364dd667
Enrique Silva <esilvam1580@gmail.com>
2025-06-06 10:51:39 -0600

    Se creo componente lista de tareas, borrar componentes autogenerados y se crean rutas.

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="sidebar">
+        <h2>Barra Lateral</h2>
+        <ul>
+            <li><router-link to="/contador">Contador</router-link></li>
+            <li><router-link to="/lista-de-tareas">Lista de Tareas</router-link></li>
+        </ul>    
+    </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.sidebar {
+    width: 200px;
+    background-color: #f4f4f4;
+    padding: 20px;
+}
+
+</style>
---
+++ src/views/MainView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+    <Sidebar></Sidebar>
+        <div class="content">
+            <router-view></router-view>  
+        </div>
+    
+    </div>
+
+</template>
+
+<script setup>
+import router from '@/router';
+import Sidebar from '../components/Sidebar.vue';
+
+</script>
+
+<style scoped>
+.container {
+    display: flex; 
+
+}
+.content {
+    flex: 1;
+    padding: 20px;
+}
+</style>
