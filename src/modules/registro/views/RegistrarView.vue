969921e0ad6a5318913059792f08a9f474cce522
Enrique Silva <esilvam1580@gmail.com>
2025-06-09 20:34:52 -0600

    Crear Store de Pina para almacenar los datos de registro

--- src/modules/registro/views/RegistrarView.vue
+++ src/modules/registro/views/RegistrarView.vue
@@ -4,13 +4,13 @@
         <Form :validation-schema="schema" @submit="onSubmit">
             <div class="form">
                 <label for="nombre">Nombre:</label>
-                <Field type="text" name="nombre" id="nombre" placeholder="Nombre" /> 
+                <Field v-model="nombre" type="text" name="nombre" id="nombre" placeholder="Nombre" /> 
                 <ErrorMessage name="nombre"></ErrorMessage>
             </div>
 
             <div class="form">
                 <label for="correo">Correo:</label>
-                <Field type="email" name="correo" id="correo" placeholder="Correo Electrónico" />            
+                <Field v-model="email" type="email" name="correo" id="correo" placeholder="Correo Electrónico" />            
                 <ErrorMessage name="correo"></ErrorMessage>    
             </div>
 
@@ -26,11 +26,18 @@
 <script setup>
 import { Form, Field, ErrorMessage } from 'vee-validate';
 import { schema } from '../schemas/validationSchema';
+import { useRegistrarStore } from '../stores/registrarStore';
+import { ref } from 'vue';
+
+const registrarStore = useRegistrarStore();
+
+const nombre = ref('');
+const email = ref('');
 
 const onSubmit = (values) => {
+    registrarStore.guardarRegistro(nombre.value, email.value);
     console.log('Formulario enviado con los siguientes valores:', values);
-    
-    // Aquí puedes agregar la lógica para enviar los datos al servidor
+
 };
 </script>
 
